ac7a53586393dcd12496b2ddd2e77ea05e203778
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2020-07-30 12:37:56 -0700

    Update App.mjs

--- src/App.mjs
+++ src/App.mjs
@@ -47,7 +47,7 @@ export default ({ title, pages, content, style, base, location }) => `
         <main class="flex-1 relative z-0 overflow-y-auto focus:outline-none" tabindex="0">
           <div class="max-w-screen-lg px-4 sm:px-6 md:px-8 pb-12">
             <div class="py-6">
-              <a class="py-24" href="https://${getGithubLink(location)}">
+              <a href="https://${getGithubLink(location)}">
                 📝 ${getGithubLink(location)}
               </a>
               <div>
