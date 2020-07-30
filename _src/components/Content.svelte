944500142efb1bb92e69f81ca51b7db2ee944dd0
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2020-07-30 02:20:01 -0700

    cleanup work

--- _src/components/Content.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-  export let html;
-</script>
-
-<div>
-  {@html html}
-</div>
--- _src/components/Error.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-  export let message;
-</script>
-
-<div>
-  <h1>Oopsies..</h1>
-  <p>{ message }</p>
-</div>
