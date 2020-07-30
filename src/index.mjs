b819b95c436f2386e02c7b1f1f225dc8d348fce0
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2020-07-30 09:49:48 -0700

    Update index.mjs

--- src/index.mjs
+++ src/index.mjs
@@ -20,7 +20,7 @@ const savePage = ({ location, title, content, anchors }) => {
     path.join(savePath, "index.html"),
     App({ title, pages, content, anchors, style, base, location })
   );
-  console.log(`Saved '${title}' as '${path.join(savePath, "index.html")}'!`, JSON.stringify(pages, null, 2));
+  console.log(`Saved '${title}' as '${path.join(savePath, "index.html")}'!`);
 };
 
 const createHome = (root) => {
